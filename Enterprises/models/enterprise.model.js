a33c1c12e9b78f72a1d127c29b731fabd4305566
areyes107 <angelmrg2000@gmail.com>
2020-03-05 23:44:23 -0600

    Enterprises/models/enterprise.model: create

---
+++ Enterprises/models/enterprise.model.js
@@ -0,0 +1,35 @@
+'use strict'
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var enterpriseSchema = Schema({
+    name: String,
+    phone: Number,
+    email: String,
+    address: String,
+    username: String,
+    password: String,
+
+    products: [{
+        name: String,
+        quantity: Number,
+    }],
+
+    employees: [{name: String,
+        charge : String,
+        department: String,
+        phoneNumber: Number,
+        email: String}],
+
+    branches:[{
+        branchName: String,
+        branchPhone: Number,
+        branchAddress: String,
+        
+         
+    }]
+
+
+});
+
+module.exports = mongoose.model('enterprise', enterpriseSchema);
